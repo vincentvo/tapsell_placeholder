5f19846a8e481c7dcd42d183af41dd5135db8ad2
vincent <vincentvo@github.com>
2014-03-22 20:05:52 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TapsellPlaceholder::Application.config.secret_key_base = '9ba7630827e05af9281dc8244ea5caeac6de25ff9f220f65699c5a0b88f2730b66d826bd1adb840b995c347f6dce0e38b552375cc086281ca5c6a6731110b261'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TapsellPlaceholder::Application.config.session_store :cookie_store, key: '_tapsell_placeholder_session'
